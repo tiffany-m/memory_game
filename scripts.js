b4f97d23d28c0196c232f28cb039d7773f8ca04c
Tiffany <tiffmollica@gmail.com>
2021-07-01 10:17:35 -0400

    game logic and bug fixes

--- scripts.js
+++ scripts.js
@@ -1,21 +1,25 @@
 const cards = document.querySelectorAll('.memory-card')
 
 let hasFlippedCard = false
+let lockBoard = false
 let firstCard, secondCard
 
 function flipCard() {
+  if(lockBoard) return; // locking board so cards can flip back if they do not match without letting you click on another card
+  if(this === firstCard) return //
+
   this.classList.toggle('flip');
-  
-  if(!hasFlippedCard){
+
+  if (!hasFlippedCard) {
     //first click
-    hasFlippedCard = true
-    firstCard = this
+    hasFlippedCard = true;
+    firstCard = this;
   } else {
     //second click
-    hasFlippedCard = false
-    secondCard = this
+    hasFlippedCard = false;
+    secondCard = this;
 
-    checkForMatch()
+    checkForMatch();
   }
 }
 
@@ -33,16 +37,37 @@ function checkForMatch() {
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+  resetBoard()
 }
 
 function unFlipCards() {
+  lockBoard = true; // locking board so cards can flip back if they do not match without letting you click on another card
+
   setTimeout(() => {
-  firstCard.classList.remove('flip');
-  secondCard.classList.remove('flip');
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard()
   }, 1500);
 }
 
+function resetBoard() {
+  hasFlippedCard = false
+  lockBoard = false
+
+  firstCard = null
+  secondCard = null
+}
+
+// wrapping function in () followed by () makes it an immediately invoked function expression, so it will be executed right after its definition
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12) // generates random numbers from 0 to 11
+    card.style.order = randomPos // order is flex box property, each flex item defaults to 0, giving each item new number sorts them in ascending order
+  })
+})()
+
 cards.forEach((card) => card.addEventListener('click', flipCard));
 
-
-27.33
+//27min
